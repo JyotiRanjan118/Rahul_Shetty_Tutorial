807dd3a2e1657b36eb5611c817cde05d7f880625
Jyoti Nath <jyoti.nath@publicissapient.com>
2022-03-27 15:28:55 +0530

    Price validation done

--- src/test/java/RealTimePRactice/JavaScriptScrolling.java
+++ src/test/java/RealTimePRactice/JavaScriptScrolling.java
@@ -1,29 +1,41 @@
 package RealTimePRactice;
 
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.WebDriverRunner;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 public class JavaScriptScrolling {
 	
 	@Test
 	public void scrollTable() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
 		
-		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		open("https://rahulshettyacademy.com/AutomationPractice/");
 		
-		JavascriptExecutor js=(JavascriptExecutor)driver;
+		JavascriptExecutor js=(JavascriptExecutor)WebDriverRunner.getWebDriver();
 		
-		js.executeScript("window.scrollBy(0,600)");
+		js.executeScript("window.scrollBy(0,700)");
 		Thread.sleep(3000);
-		
 		js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=5000");
 		
+		$$(".tableFixHead td:nth-of-type(4)").stream().map(b->b.getText()).forEach(System.out::println);
 		
+		ElementsCollection listPrice=$$(".tableFixHead td:nth-of-type(4)");
+		int sum=0;
+
+		for(int i=0;i<listPrice.size();i++) {
+			
+			sum=sum+Integer.parseInt(listPrice.get(i).getText());
+		}
+		
+		System.out.println(sum);
+		int total=Integer.parseInt($("div.totalAmount").getText().split(":")[1].trim());
+		
+		Assert.assertEquals(sum, total);
 	}
 
 }
